324fdc079dd3a77d02f5a8ac611f15c910de60ed
hukish <hudsonhukish@gmail.com>
2020-05-08 21:09:09 +0300

    modify changes

--- app/main/views.py
+++ app/main/views.py
@@ -4,10 +4,18 @@ from ..requests import get_quotes
 
 @main.route('/')
 def index():
-    '''
-    View root page.
-    '''
 
-    
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title =     title = 'Home - Welcome to The Quote review Website Online'
+    return render_template('index.html',title = title)
 
-    return render_template('index.html', title=title)
+
+@main.route('/quote/<int:quote_id>')
+def movie(quote_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('quote.html',id = quote_id)
