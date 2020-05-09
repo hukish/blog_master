9404006162e6b4c702d4b5fcf37e6f32ff763c90
hukish <hudsonhukish@gmail.com>
2020-05-09 19:31:46 +0300

    modify changes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,83 @@
+from . import db
+
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Quote:
 
     def __init__(self, id, author, quote):
