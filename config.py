4ddae87ec07fcdd1828a9c35c315f11a1ba72b40
hukish <hudsonhukish@gmail.com>
2020-05-09 21:59:27 +0300

    added email configurations

--- config.py
+++ config.py
@@ -10,6 +10,14 @@ class Config:
     QUOTE_API_BASE_URL = os.environ.get('QUOTE_API_BASE_URL')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hudson:1234@localhost/mblog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
    
 class ProdConfig(Config):
     '''
