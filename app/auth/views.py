c71ab662d9d20ed5126a6ecd52329e419acabf48
hukish <hudsonhukish@gmail.com>
2020-05-09 20:38:26 +0300

    update changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from flask import render_template,redirect,url_for
 from ..models import User
 from .forms import RegistrationForm
 from .. import db
+from flask_login import login_user,logout_user,login_required
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -19,6 +20,13 @@ def login():
     title = "blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+    
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
