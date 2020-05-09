6e4bf57a09332549a3dd4f614f3a54de06526789
hukish <hudsonhukish@gmail.com>
2020-05-09 22:01:02 +0300

    initialized the email

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 
@@ -16,6 +17,7 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 
 def create_app(config_name):
@@ -29,6 +31,10 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+     mail.init_app(app)
+
+
+     
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
  export QUOTE_API =  'http://quotes.stormconsultancy.co.uk/random.json'
   
 export SECRET_KEY=1234
+export MAIL_USERNAME=<Your Email Address>
+export MAIL_PASSWORD=<Your Email Password>
+python3.6 manage.py server
 
 python3.6 manage.py server
