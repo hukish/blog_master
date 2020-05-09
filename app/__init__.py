2ed94d1baef971fbcfff2cb68e7ac155a0161ca9
hukish <hudsonhukish@gmail.com>
2020-05-09 22:17:05 +0300

    added and intialized simpleMDE

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
-
+from flask_simplemde import SimpleMDE
 
 
 
@@ -18,7 +18,7 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
-
+simple = SimpleMDE()
 
 def create_app(config_name):
 
@@ -31,10 +31,12 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-     mail.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+ 
 
 
-     
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
