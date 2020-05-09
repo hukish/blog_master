8d2434a69ae86c3d11e85744052e91a02a94310e
hukish <hudsonhukish@gmail.com>
2020-05-09 21:57:24 +0300

    modify changes

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..requests import get_quotes
 from flask_login import login_required
 from ..models import User
 from .forms import UpdateProfile
-from .. import db
+from .. import db,photos
 
 @main.route('/')
 def index():
@@ -39,6 +39,27 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
